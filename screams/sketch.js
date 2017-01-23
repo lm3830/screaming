55259794462cf126aa20b7636e098dc41d434bf9
lm3830 <lm3830@trevor.org>
2017-01-23 08:56:58 -0500

    Add files via upload

---
+++ screams/sketch.js
@@ -0,0 +1,124 @@
+var x=50;
+var y=50;
+var x2;
+var y2;
+var x3;
+var y3;
+var weird;
+var back
+function setup() {
+  createCanvas(36*40,24*40);
+	frameRate(10);
+	weird.play();
+	weird.play();
+		weird.play();
+		weird.play();
+		imageMode(CENTER);
+}
+function preload(){
+	weird = loadSound('assets/bestfriend.mp3');
+	back = loadImage('assets/hi.png')
+}
+function draw() {
+textSize(25);
+	text("Warning: This piece has loud sounds and flashing colors. Press any key to proceed", 250,500,2000);
+	text("'Your Best Friend' © Toby Fox 2015. This is not for profit and use is protected under Fair Use", 250,750,2000)
+if(keyIsPressed===true){
+	image(back,36*40,24*40,60);
+  var r=random(200,250);
+var g=random(200,250);
+var b=random(200,250);
+
+ 	fill(0,0,0,60);
+ 	stroke(0,0,0);
+ 	text("me", x,y,50);
+  fill(r,250-random(0,50),0,60)
+ x3=x+random(1,1000);
+ 	x2=x-random(1,1000);
+ 	y3=y+random(1,1000);
+ 	y2=y-random(1,1000);
+ 	
+ 	stroke(random(1,250),random(1,250),random(1,250));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+ fill(r,0,0,60);
+ text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+ rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+ ellipse(random(x3,x2),random(y3,y2),random(1,100));text("them",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+	text("them",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+	text("them",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+   text("them",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+    text("him",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+ text("him",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+	fill(0,g,0,60);
+	
+  
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+   text("him",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+    text("him",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+    
+       text("him",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+    fill(r,250-random(0,100),0,60)
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  fill(0,g,250-random(0,50),60);
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  fill(0,250-random(0,50),b,60);
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+  fill(r-random(0,50),0,b,60);
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("you",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("her",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("her",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  
+  text("her",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+   text("him",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+  text("him",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+   text("him",random(x3,x2),random(y3,y2),random(1,100),random(1,100));
+ 
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+   ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  ellipse(random(x3,x2),random(y3,y2),random(1,100));
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+  
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+  rect(random(x3,x2),random(y3,y2),random(1,100),random(1,100),random(1,100));
+ 	y3-=100;
+ 	y2+=100;
+ 	x2+=100;
+ 	x3-=100;
+ 	x=mouseX;
+ 	y=mouseY;
+}
+}
